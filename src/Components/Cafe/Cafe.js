2ae9af80e48497f9ae9266ebb05120585f9f80f7
Jacob G <jrgrant560@gmail.com>
2023-10-04 13:07:55 -0400

    installed bootstrap dependency; started building component

--- src/Components/Cafe/Cafe.js
+++ src/Components/Cafe/Cafe.js
@@ -1,8 +1,18 @@
 import React from "react";
 import CafeStyle from "./Cafe.module.css";
+import {Button, Image} from "react-bootstrap";
+import Logo from "../../images/icon.png";
 
 const Cafe = () => {
     return(<>
+    <div>
+        <Image/>
+        <h1>Services From </h1>
+        <h1>The Heart of God </h1>
+        <div>
+            {/* STOPPED AT 12:32 ON TUTORIAL VIDEO */}
+        </div>
+    </div>
     </>)
 }
 
