7d2f0813b1aec074b0f082226f5cc52c5a541692
Jacob G <jrgrant560@gmail.com>
2023-10-05 13:12:11 -0400

    completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Cafe from './Components/Cafe/Cafe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <Cafe></Cafe>
+    </>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- src/Components/Cafe/Cafe.js
+++ src/Components/Cafe/Cafe.js
@@ -1,18 +1,27 @@
 import React from "react";
 import CafeStyle from "./Cafe.module.css";
-import {Button, Image} from "react-bootstrap";
-import Logo from "../../images/icon.png";
+import { Button, Image } from "react-bootstrap";
+import logoImg from "../../images/icon.png";
+import diamondImg from "../../images/diamondIcon.png";
 
 const Cafe = () => {
-    return(<>
-    <div>
-        <Image/>
-        <h1>Services From </h1>
-        <h1>The Heart of God </h1>
-        <div>
-            {/* STOPPED AT 12:32 ON TUTORIAL VIDEO */}
+
+    // const logoStyle = CafeStyle.logoStyle;
+
+    return (<>
+        <div className={CafeStyle.wrapper}>
+            <Image src={logoImg} className={CafeStyle.logoImageStyle} />
+            <h1 className={CafeStyle.h1Style}>Services From </h1>
+            <h1 className={CafeStyle.h1Style}>The Heart of God </h1>
+            <div className={CafeStyle.listWrapper}>
+                <p style={{ margin: "0" }}>Page A</p>
+                <p style={{ margin: "0" }}>Page B</p>
+                <p style={{ margin: "0" }}>Page C</p>
+                <p style={{ margin: "0" }}>Page D</p>
+            </div>
+            <Image src={diamondImg} className={CafeStyle.diamondImageStyle} />
+            <Button className={CafeStyle.learnButton}>LEARN MORE</Button>
         </div>
-    </div>
     </>)
 }
 
